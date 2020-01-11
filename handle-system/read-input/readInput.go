29e055b824f6474ed50fb956a9461d89fd3f810c
Pengyuan Zhou <jackson.sun@163.com>
2020-01-11 20:06:35 +0800

    upload test input message

--- handle-system/read-input/readInput.go
+++ handle-system/read-input/readInput.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 const systemargslen = 3
 
 func main() {
 	getInputFromCommandLine()
+	getInputFromInteractive()
+
+}
+
+func getInputFromInteractive() {
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Simple Shell")
+	fmt.Println("---------------------")
+
+	for {
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+
+		if strings.Compare("hi", text) == 0 {
+			fmt.Println("hello, Yourself")
+		} else if strings.Compare("exit", text) == 0 {
+			fmt.Println("see you")
+			break
+		}
+
+	}
 
 }
 
